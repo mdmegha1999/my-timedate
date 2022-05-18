249979c0ad888841b421baa60899ea95d29f98e8
mdmegha1999 <94183187+mdmegha1999@users.noreply.github.com>
2022-05-18 08:21:02 +0530

    timedate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
+import React,{useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let time = new Date().toLocaleTimeString()
+  const [currentTime, setCurrentTime] = useState(time);
+  const UpdateTime = () =>{
+    time = new Date().toLocaleTimeString();
+    setCurrentTime(time);
+  }
+  setInterval(UpdateTime, 1000);
+  let todayDate = new Date().toLocaleDateString()
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Clock</h1>
+        <div className='samay'>
+        <h2 className='time'>Time<br/>{currentTime}</h2>
+         <h2 className='date'>Date <br/>{todayDate}</h2> 
+         </div>
+      </div>
     </div>
   );
 }
